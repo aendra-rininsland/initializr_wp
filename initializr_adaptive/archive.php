56edc80de45f636ef209987f3a9a4673a2bb2348
Ændrew Rininsland <aendrew@windowlicker.local>
2012-10-04 00:37:29 +0100

    Removed all remaining mentions of TwentyEleven from code.

--- initializr_adaptive/archive.php
+++ initializr_adaptive/archive.php
@@ -22,13 +22,13 @@ get_header(); ?>
 				<header class="page-header">
 					<h1 class="page-title">
 						<?php if ( is_day() ) : ?>
-							<?php printf( __( 'Daily Archives: %s', 'twentyeleven' ), '<span>' . get_the_date() . '</span>' ); ?>
+							<?php printf( __( 'Daily Archives: %s', 'initializr_adaptive' ), '<span>' . get_the_date() . '</span>' ); ?>
 						<?php elseif ( is_month() ) : ?>
-							<?php printf( __( 'Monthly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php printf( __( 'Monthly Archives: %s', 'initializr_adaptive' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'initializr_adaptive' ) ) . '</span>' ); ?>
 						<?php elseif ( is_year() ) : ?>
-							<?php printf( __( 'Yearly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php printf( __( 'Yearly Archives: %s', 'initializr_adaptive' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'initializr_adaptive' ) ) . '</span>' ); ?>
 						<?php else : ?>
-							<?php _e( 'Blog Archives', 'twentyeleven' ); ?>
+							<?php _e( 'Blog Archives', 'initializr_adaptive' ); ?>
 						<?php endif; ?>
 					</h1>
 				</header>
@@ -54,11 +54,11 @@ get_header(); ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'initializr_adaptive' ); ?></h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'initializr_adaptive' ); ?></p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
