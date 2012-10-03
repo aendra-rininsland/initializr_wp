bffe60c139bc66bc3087a6614f86c79acc019af1
Ændrew Rininsland <aendrew@windowlicker.local>
2012-10-04 00:06:45 +0100

    Random bug fixes.

--- initializr_adaptive/archive.php
+++ initializr_adaptive/archive.php
@@ -33,7 +33,7 @@ get_header(); ?>
 					</h1>
 				</header>
 
-				<?php twentyeleven_content_nav( 'nav-above' ); ?>
+				<?php initializr_adaptive_content_nav( 'nav-above' ); ?>
 
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -48,7 +48,7 @@ get_header(); ?>
 
 				<?php endwhile; ?>
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+				<?php initializr_adaptive_content_nav( 'nav-below' ); ?>
 
 			<?php else : ?>
 
